e576cf26fac216da245e44a652b11fa017ae2ebd
JuanM-DS <20230676@itla.edu.do>
2024-07-11 17:53:54 -0400

    Creating the Identity context and custom entities

---
+++ Restaurant.Infrastructure.Identity/Context/RestaurantIdentityDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Infrastructure.Identity.Entities;
+
+namespace Restaurant.Infrastructure.Identity.Context
+{
+    public class RestaurantIdentityDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public RestaurantIdentityDbContext(DbContextOptions<RestaurantIdentityDbContext> option)
+            : base(option)
+        {}
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //foreach (var item in ChangeTracker.Entries<AuditableBaseEntity>)
+            //{
+
+            //}
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("Identity");
+
+            #region entities configurations
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+                
+                entity.Property(e => e.FirstName);
+                entity.Property(e => e.LastName);
+
+                entity.HasMany(e => e.Roles)
+                      .WithMany(f => f.Users)
+                      .UsingEntity<IdentityUserRole<string>>();
+            });
+
+            builder.Entity<ApplicationRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles");
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins");
+            });
+            #endregion
+        }
+    }
+}
